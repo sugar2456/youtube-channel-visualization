a003d6aea8f9d36fbf66e9bb1f914712710c2736
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-23 00:10:05 +0900

    コンポーネントフォルダ位置を変更

--- __tests__/unit/app/components/Header.test.tsx
+++ __tests__/unit/app/components/Header.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
-import Header from "@/components/common/Header";
+import Header from "@/app/components/Header";
 
 describe("Header", () => {
   it("ヘッダーが正しくレンダリングされる", () => {
@@ -8,7 +8,7 @@ describe("Header", () => {
 
     // ヘッダーが正しく表示されているか確認(大文字小文字は区別しない)
     const headerElement = screen.getByRole("heading", {
-      name: /GitHub Repository Viewer/i,
+      name: /Youtube Channel Search/i,
     });
     expect(headerElement).toBeInTheDocument();
 
@@ -19,7 +19,7 @@ describe("Header", () => {
 
   it('ヘッダーのリンクが検索画面のリンクになっている', () => {
     render(<Header />);
-    const linkElement = screen.getByRole('link', { name: /GitHub Repository Viewer/i });
-    expect(linkElement).toHaveAttribute('href', '/search');
+    const linkElement = screen.getByRole('link', { name: /Youtube Channel Search/i });
+    expect(linkElement).toHaveAttribute('href', '/');
   });
 });
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="bg-green-600 text-white p-4">
       <h1 className="text-4xl font-bold">
-        <Link href="/search">GitHub Repository Viewer</Link>
+        <Link href="/">Youtube Channel Search</Link>
       </h1>
     </header>
   );
